a834e8a2c5fcff698188a72370d529bc691a0f0d
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-24 12:38:43 -0500

    added llama output function

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, render_template
-
+from dotenv import load_dotenv
 import os
+import json
+from llamaapi import LlamaAPI
 
 app = Flask(__name__)
-os.getenv("LLAMA_API")
+load_dotenv
+api_key = os.getenv("LLAMA_API")
+llama = LlamaAPI(api_key)
 
 def generate_lorem_ipsum_text():
     # Generate lorem ipsum text
@@ -14,9 +18,23 @@ def generate_lorem_ipsum_text():
 def index():
     if request.method == "POST":
         user_input = request.form["user_input"]
+        llama_output = get_llama_output(user_input)
         lorem_ipsum_text = generate_lorem_ipsum_text()
         return render_template("index.html", user_input=user_input, lorem_ipsum_text=lorem_ipsum_text)
     return render_template("index.html")
 
 
-
+def get_llama_output(user_input):
+    api_request_json = {
+        "model": "llama3.1-70b",
+        "messages": [
+            {"role": "user", "content": "Explain the math behind money betting lines and what over/under means?"},
+        ]
+    }
+    # Execute the Request
+    response = llama.run(api_request_json)
+    try:
+        print(json.dumps(response.json(), indent=2))
+    except:
+        print("\n\n\n\n\n " + response.text)
+        return response.json()
