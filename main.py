c22612cb07dc40993c6c639c3101c8df9e2f2cd5
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-24 11:04:27 -0500

    index.html page added

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+def generate_lorem_ipsum_text():
+    # Generate lorem ipsum text
+    lorem_ipsum_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    return lorem_ipsum_text
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+        lorem_ipsum_text = generate_lorem_ipsum_text()
+        return render_template("index.html", user_input=user_input, lorem_ipsum_text=lorem_ipsum_text)
+    return render_template("index.html")
+
+
 
