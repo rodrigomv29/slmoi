c818730aa29051163d73d8d04fcaa766bd0a5d5f
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-26 11:42:07 -0500

    added news for llm function calling

---
+++ news_generator.py
@@ -0,0 +1,11 @@
+import os
+import requests
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("NEWS_API")
+url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+response = requests.get(url)
+for i in range(len(response.json()['articles'])):
+    print(response.json()["articles"][i]['title'])
