ca9c5fb1572f62a38bd594416b2dc6b033e99e0b
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-27 15:16:25 -0500

    modified url for function calling implementation

--- news_generator.py
+++ news_generator.py
@@ -7,7 +7,10 @@ load_dotenv()
 api_key = os.getenv("NEWS_API")
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
 
-def get_news_headlines():
-    response = requests.get(url)
+def get_news_headlines(category):
+    modified_url = url
+    modified_url += f"&category={category}"
+    response = requests.get(modified_url)
     for i in range(len(response.json()['articles'])):
         print(response.json()["articles"][i]['title'])
+get_news_headlines("business")
