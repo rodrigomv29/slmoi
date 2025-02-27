0397d1a39977a925a3c916dc8ad64438b508a127
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-27 14:17:59 -0500

    testing llm api key

--- function_calling.py
+++ function_calling.py
@@ -1,5 +1,24 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-client = OpenAI()
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-print(type(client))
+client = OpenAI(
+    api_key = openai_api_key,
+    base_url = "https://api.llama-api.com"
+)
+
+response = client.chat.completions.create(
+    model="llama3.1-70b",
+    messages=[
+        {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
+        {"role": "user", "content": "What are the news today?"}
+    ],
+    
+
+)
+
+print(response.model_dump_json(indent=2))
+print(response.choices[0].message.content)
