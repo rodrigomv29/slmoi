60af1dd51935180687909d49fc2825369825623d
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-28 13:40:42 -0500

    function calling enums added

--- function_calling.py
+++ function_calling.py
@@ -11,7 +11,7 @@ client = OpenAI(
     base_url = "https://api.llama-api.com"
 )
 messages=[
-        {"role": "user", "content": "What are the sports news today?"}
+        {"role": "user", "content": "Is Vladmir Putin on the news today?"}
 ]
 model="llama3.1-70b"
 tools = [{
@@ -22,7 +22,18 @@ tools = [{
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "category": {"type": "string"},
+                    "category": {
+                        "type": "string",
+                        "enum": [
+                            "business",
+                            "entertainment",
+                            "general",
+                            "health",
+                            "science",
+                            "sports",
+                            "technology"
+                        ]
+                    },
                 },
                 "required": ["category"],
                 "additionalProperties": False
@@ -38,9 +49,26 @@ completion = client.chat.completions.create(
 )
 
 
-tool_call = completion.choices[0].message.tool_calls
-json_list = json.loads(tool_call)
-args = json_list[0]['function']['arguments']
-arg_json = json.loads(args)
-result = news_generator.get_news_headlines(arg_json["category"])
-print(result)
+tool_call_str = completion.choices[0].message.tool_calls
+if tool_call_str is None:
+    print(completion.choices[0].message.content)
+else:
+    tool_call = json.loads(tool_call_str)
+    # print(json.dumps(tool_call, indent=2))
+    args = tool_call[0]['function']['arguments']
+    arg_json = json.loads(args)
+    result = news_generator.get_news_headlines(arg_json["category"])
+    messages.append(completion.choices[0].message)
+
+    messages.append({                               # append result message
+        "role": "tool",
+        "tool_call_id": tool_call[0]['id'],
+        "content": str(result)
+        })
+    completion_2 = client.chat.completions.create(
+        model="llama3.1-70b",
+        messages=messages,
+        tools=tools,
+    )
+
+    print(completion_2.choices[0])
