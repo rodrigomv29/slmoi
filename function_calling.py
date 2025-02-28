a139c7bf8666bf2ace369fc48d2e2747c5d7ff1e
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-28 08:12:54 -0500

    function calling for better searches

--- function_calling.py
+++ function_calling.py
@@ -1,7 +1,8 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
-
+import news_generator
+import json
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
@@ -9,16 +10,37 @@ client = OpenAI(
     api_key = openai_api_key,
     base_url = "https://api.llama-api.com"
 )
-
-response = client.chat.completions.create(
+messages=[
+        {"role": "user", "content": "What are the sports news today?"}
+]
+model="llama3.1-70b"
+tools = [{
+        "type": "function",
+        "function": {
+            "name": "get_news_headlines",
+            "description": "Get current news headlines given a news category",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "category": {"type": "string"},
+                },
+                "required": ["category"],
+                "additionalProperties": False
+        },
+        "strict": True
+        }
+    }
+]
+completion = client.chat.completions.create(
     model="llama3.1-70b",
-    messages=[
-        {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
-        {"role": "user", "content": "What are the news today?"}
-    ],
-    
-
+    messages=messages,
+    tools=tools,
 )
 
-print(response.model_dump_json(indent=2))
-print(response.choices[0].message.content)
+
+tool_call = completion.choices[0].message.tool_calls
+json_list = json.loads(tool_call)
+args = json_list[0]['function']['arguments']
+arg_json = json.loads(args)
+result = news_generator.get_news_headlines(arg_json["category"])
+print(result)
