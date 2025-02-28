876a7496b65b22ae2809b7a80354f1d1540480d4
Rodrigo Morales <rodrigomv29@gmail.com>
2025-02-28 13:41:14 -0500

    categories enum added

--- news_generator.py
+++ news_generator.py
@@ -8,10 +8,15 @@ api_key = os.getenv("NEWS_API")
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
 
 def get_news_headlines(category):
+    categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+    if category not in categories:
+        return "NOT A CATEGORY!"
     modified_url = url
     modified_url += f"&category={category}"
     response = requests.get(modified_url)
     result = []
     for i in range(len(response.json()['articles'])):
         result.append(response.json()["articles"][i]['title'])
-    return result
+    return str(result)
+if __name__ == "__main__":
+    print(get_news_headlines("penguin"))
