94c518d22cc359e477430c35585e05289427fa71
Rodrigo Morales <rodrigomv29@gmail.com>
2025-03-21 15:23:49 -0400

    main method removed

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
-from dotenv import load_dotenv
-import os
-import sqlite3
-from datetime import datetime
-from openai import OpenAI
-import function_calling
 
 app = Flask(__name__)
-load_dotenv()
-api_key = os.getenv("LLAMA_API")
 
 @app.route("/")
 def index():
