1442560c6f106473a8a185283f755619877dd83c
Rodrigo Morales <rodrigomv29@gmail.com>
2025-03-30 19:35:39 -0400

    raise openai error

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from dotenv import load_dotenv
 import os
 import sqlite3
 from datetime import datetime
-from openai import OpenAI
+from openai import OpenAI, OpenAIError
 import function_calling
 
 app = Flask(__name__)
 # load_dotenv()
 api_key = os.environ.get("OPENAI_API_KEY")
-print(api_key)
 base_url = "https://api.llama-api.com"
-client = OpenAI(
-api_key = api_key,
-base_url = "https://api.llama-api.com"
-)
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    print("Works?")
-    return "<h1>Hello World!</h1>"
+try:
+    client = OpenAI(
+    api_key = api_key,
+    base_url = "https://api.llama-api.com"
+    )
+except OpenAIError:
+    @app.route("/", methods=["GET", "POST"])
+    def index():
+        return "<h1>Hello World!</h1>"
+    ## run hello world webpage
+    
