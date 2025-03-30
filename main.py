612abeac3222065d692d7dcdb6e637885f4f7a6a
Rodrigo Morales <rodrigomv29@gmail.com>
2025-03-29 20:56:24 -0400

    adding openai client

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, request, render_template, redirect, url_for
+from dotenv import load_dotenv
+import os
+import sqlite3
+from datetime import datetime
+from openai import OpenAI
+import function_calling
 
 app = Flask(__name__)
+load_dotenv()
+api_key = os.getenv("LLAMA_API")
+base_url = "https://api.llama-api.com"
+client = OpenAI(
+api_key = api_key,
+base_url = "https://api.llama-api.com"
+)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return "<h1>Hello World!</h1>"
-
