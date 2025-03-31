39add4b1bd46145f5b1aa3379e4c7c33e61820c2
Rodrigo Morales <rodrigomv29@gmail.com>
2025-03-31 10:19:52 -0400

    absolute path names for db connections

--- main.py
+++ main.py
@@ -14,24 +14,26 @@ client = OpenAI(
 api_key = api_key,
 base_url = "https://api.llama-api.com"
 )
-"""
+
 def init_db():
-    conn = sqlite3.connect('prompts.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS user_inputs
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  input_text TEXT NOT NULL,
-                  date TEXT NOT NULL,
-                  user_name TEXT DEFAULT 'guest')''')
-    conn.commit()
-    conn.close()
-"""
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "prompts.db")
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS user_inputs
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    input_text TEXT NOT NULL,
+                    date TEXT NOT NULL,
+                    user_name TEXT DEFAULT 'guest')''')
+        conn.commit()
+        conn.close()
+
 def get_llama_output(inp, fun_call):
     if fun_call == 1:
         response = client.chat.completions.create(
         model="llama3.1-70b",
         messages=[
-            {"role": "system", "content": "You are a programming assistant that doesn't fully answer questions but recommends computer science textbooks to read for a deeper understanding"},
+            {"role": "system", "content": "You are a helpful assistant that answer questions in a grandiloquent  way"},
             {"role": "user", "content": inp}
         ],
     )
@@ -55,24 +57,25 @@ def index():
             print("NEWS!!")
         llama_output = get_llama_output(user_input, fun_call=1 )
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        """conn = sqlite3.connect('prompts.db')
-        c = conn.cursor()
-        c.execute("INSERT INTO user_inputs (input_text, date, user_name) VALUES (?, ?, ?)",
-                  (user_input, date, user_name))
-        conn.commit()
-        conn.close()
-        """
-
-        return render_template('index.html', user_input=user_input, llama_output=llama_output)
-    return render_template('index.html')
-    #else:
-"""conn = sqlite3.connect('prompts.db')
-        c = conn.cursor()
-        c.execute("SELECT * FROM user_inputs")
-        rows = c.fetchall()
-        conn.close()
-        return render_template('index.html', rows=rows)"
-        """
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(BASE_DIR, "prompts.db")
+        with sqlite3.connect(db_path) as conn:        
+            c = conn.cursor()
+            c.execute("INSERT INTO user_inputs (input_text, date, user_name) VALUES (?, ?, ?)",
+                    (user_input, date, user_name))
+            conn.commit()
+            conn.close()
+            return render_template('index.html', user_input=user_input, llama_output=llama_output)
+    else:
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(BASE_DIR, "prompts.db")
+        with sqlite3.connect(db_path) as conn:
+            c = conn.cursor()
+            c.execute("SELECT * FROM user_inputs")
+            rows = c.fetchall()
+            conn.close()
+            return render_template('index.html', rows=rows)
+        
 
 if __name__ == '__main__':
     # init_db()
