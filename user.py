83d45e72d6683c2afb753d62ce7d6c02b372210e
Rodrigo Morales <rodrigomv29@gmail.com>
2025-06-11 20:06:16 -0400

    user information added

---
+++ user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, name, password, birthday):
+        self.name = name
+        self.password = password
+        self.birthday = birthday
--- user_prompt.py
+++ user_prompt.py
@@ -1,3 +1,5 @@
 class UserPrompt:
-    def __init__(self, prompt):
+    def __init__(self, prompt, output, username):
         self.prompt = prompt
+        self.output = output
+        self.username = username
