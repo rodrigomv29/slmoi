92a758a8cbe11b85e12d52f84627b80ac9c8d133
Rodrigo Morales <rodrigomv29@gmail.com>
2025-09-29 19:56:25 -0400

    feature: conversation feature

---
+++ conversation.py
@@ -0,0 +1,12 @@
+class Conversation:
+    def __init__(self, user_input, output, user_name="guest"):
+        self.user_name = user_name
+        self.user_input = user_input
+        self.output = output
+    def get_user_name(self):
+        return self.user_name
+    def get_user_input(self):
+        return self.user_input
+    def get_output(self):
+        return self.output
+    
