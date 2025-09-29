a5bafd0c6dea756c9b5f5ab87a3ccf27232a3030
Rodrigo Morales <rodrigomv29@gmail.com>
2025-09-29 18:55:51 -0400

    add: user input class

--- user.py
+++ user.py
@@ -13,3 +13,11 @@ class User:
         return self.email
     def get_interests(self):
         return self.interests
+class UserInput:
+    def __init__(self, inp, user_name):
+        self.inp =inp
+        self.user_name = user_name
+    def get_inp(self):
+        return self.inp
+    def get_user_name(self):
+        return self.user_name
